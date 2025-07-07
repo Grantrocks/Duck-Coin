21f621584954d71d1194044a201f0c7de7921a6d
Grantrocks <grantmac12345678911@outlook.com>
2025-07-07 01:38:31 -0400

    forgot to make sure file type ended in .sqlite

--- init/setupdb.py
+++ init/setupdb.py
@@ -1,13 +1,13 @@
 import sqlite3
 import json
 import hashlib
-con = sqlite3.connect("databases/blockchain.db")
+con = sqlite3.connect("databases/blockchain.sqlite")
 cur = con.cursor()
 cur.execute("CREATE TABLE blocks(blockHeight integer NOT NULL,blockHash text NOT NULL,header text NOT NULL,transactions text NOT NULL)")
 con.commit()
 
 
-txcon=sqlite3.connect("databases/txqueue.db")
+txcon=sqlite3.connect("databases/txqueue.sqlite")
 txcur=txcon.cursor()
 txcur.execute("CREATE TABLE txs(id text NOT NULL,json text NOT NULL)")
 txcon.commit()
